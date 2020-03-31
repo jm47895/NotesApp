9a9f7f473dc8215e5b5efbcce62f7d0b9ccb9326
jm47895 <jordan.madrigal@gmail.com>
2020-03-30 21:12:56 -0500

    Add notes adapter.

---
+++ app/src/main/java/com/jordanmadrigal/notesapp/adapters/NotesAdapter.java
@@ -0,0 +1,54 @@
+package com.jordanmadrigal.notesapp.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jordanmadrigal.notesapp.R;
+import com.jordanmadrigal.notesapp.models.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> {
+
+    private List<Note> listOfNotes = new ArrayList<>();
+
+    public NotesAdapter(List<Note> listOfNotes) {
+        this.listOfNotes = listOfNotes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_notes_list_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.title.setText(listOfNotes.get(position).getTitle());
+        holder.timestamp.setText(listOfNotes.get(position).getTimeStamp());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listOfNotes.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView title, timestamp;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.note_title);
+            timestamp = itemView.findViewById(R.id.note_timestamp);
+        }
+    }
+}
