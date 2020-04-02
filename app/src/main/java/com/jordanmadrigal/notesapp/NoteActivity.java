b0782dd34fa4543687fb9d5001fb9432efb8444d
jm47895 <jordan.madrigal@gmail.com>
2020-04-02 18:11:59 -0500

    Add double click listeners to notepad in note activity

--- app/src/main/java/com/jordanmadrigal/notesapp/NoteActivity.java
+++ app/src/main/java/com/jordanmadrigal/notesapp/NoteActivity.java
@@ -3,6 +3,9 @@ package com.jordanmadrigal.notesapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,7 +14,8 @@ import com.jordanmadrigal.notesapp.utils.LinedEditText;
 
 import static com.jordanmadrigal.notesapp.utils.Constants.NOTE_CONTENT_KEY;
 
-public class NoteActivity extends AppCompatActivity{
+public class NoteActivity extends AppCompatActivity implements
+        View.OnTouchListener, GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
 
     private static final String TAG = "NoteActivity";
 
@@ -20,6 +24,7 @@ public class NoteActivity extends AppCompatActivity{
     private TextView textTitle;
     private Note initialNote;
     private boolean isNewNote;
+    private GestureDetector gestureDetector;
 
 
     @Override
@@ -37,6 +42,13 @@ public class NoteActivity extends AppCompatActivity{
             setExistingNoteProperties();
         }
 
+        setListeners();
+
+    }
+
+    private void setListeners(){
+        linedEditText.setOnTouchListener(this);
+        gestureDetector = new GestureDetector(this, this);
     }
 
     private void setExistingNoteProperties() {
@@ -62,5 +74,56 @@ public class NoteActivity extends AppCompatActivity{
         isNewNote = true;
 
         return true;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        Log.d(TAG, "onDoubleTapEvent: double tap");
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
     }
 }
