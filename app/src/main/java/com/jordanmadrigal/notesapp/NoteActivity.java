3b74bb93e1f95fa185746bfadb40a48e54845bc3
jm47895 <jordan.madrigal@gmail.com>
2020-04-02 19:31:47 -0500

    Edit notepad UI to function with buttons

--- app/src/main/java/com/jordanmadrigal/notesapp/NoteActivity.java
+++ app/src/main/java/com/jordanmadrigal/notesapp/NoteActivity.java
@@ -1,12 +1,15 @@
 package com.jordanmadrigal.notesapp;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.jordanmadrigal.notesapp.models.Note;
@@ -14,10 +17,11 @@ import com.jordanmadrigal.notesapp.utils.LinedEditText;
 
 import static com.jordanmadrigal.notesapp.utils.Constants.NOTE_CONTENT_KEY;
 
-public class NoteActivity extends AppCompatActivity implements
-        View.OnTouchListener, GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+public class NoteActivity extends AppCompatActivity implements View.OnTouchListener, View.OnClickListener {
 
     private static final String TAG = "NoteActivity";
+    private static final int EDIT_MODE_ENABLED = 1;
+    private static final int EDIT_MODE_DISABLED = 0;
 
     private LinedEditText linedEditText;
     private EditText editTitle;
@@ -25,6 +29,9 @@ public class NoteActivity extends AppCompatActivity implements
     private Note initialNote;
     private boolean isNewNote;
     private GestureDetector gestureDetector;
+    private ImageButton checkButton, backButton;
+    private int mode;
+
 
 
     @Override
@@ -35,11 +42,15 @@ public class NoteActivity extends AppCompatActivity implements
         linedEditText = findViewById(R.id.note_body_edit);
         editTitle = findViewById(R.id.note_edit_title);
         textTitle = findViewById(R.id.note_text_title);
+        checkButton = findViewById(R.id.note_check_button);
+        backButton = findViewById(R.id.note_back_button);
 
         if(isNewNoteIntent()){
             setNewNoteProperties();
+            setEditModeEnabled(true);
         }else{
             setExistingNoteProperties();
+            setEditModeEnabled(false);
         }
 
         setListeners();
@@ -48,7 +59,8 @@ public class NoteActivity extends AppCompatActivity implements
 
     private void setListeners(){
         linedEditText.setOnTouchListener(this);
-        gestureDetector = new GestureDetector(this, this);
+        textTitle.setOnClickListener(this);
+        checkButton.setOnClickListener(this);
     }
 
     private void setExistingNoteProperties() {
@@ -68,62 +80,79 @@ public class NoteActivity extends AppCompatActivity implements
             initialNote = getIntent().getParcelableExtra(NOTE_CONTENT_KEY);
             Log.d(TAG, "isNewNote: " + initialNote.toString());
             isNewNote =false;
+            mode = EDIT_MODE_DISABLED;
 
             return false;
         }
+
         isNewNote = true;
+        mode = EDIT_MODE_ENABLED;
 
         return true;
     }
 
+    private void setEditModeEnabled(boolean editModeEnabled){
+        if(editModeEnabled){
+            checkButton.setVisibility(View.VISIBLE);
+            editTitle.setVisibility(View.VISIBLE);
+
+            backButton.setVisibility(View.GONE);
+            textTitle.setVisibility(View.GONE);
+
+            linedEditText.setCursorVisible(true);
+            mode = EDIT_MODE_ENABLED;
+        }else{
+            checkButton.setVisibility(View.GONE);
+            editTitle.setVisibility(View.GONE);
+
+            backButton.setVisibility(View.VISIBLE);
+            textTitle.setVisibility(View.VISIBLE);
+
+            linedEditText.setCursorVisible(false);
+            mode = EDIT_MODE_DISABLED;
+
+            hideKeyboard();
+        }
+    }
+
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        return gestureDetector.onTouchEvent(event);
-    }
-
-    @Override
-    public boolean onSingleTapConfirmed(MotionEvent e) {
+        setEditModeEnabled(true);
         return false;
     }
 
     @Override
-    public boolean onDoubleTap(MotionEvent e) {
-        return false;
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.note_check_button:
+                setEditModeEnabled(false);
+                break;
+            case R.id.note_text_title:
+                setEditModeEnabled(true);
+                editTitle.requestFocus();
+                editTitle.setSelection(editTitle.length());
+                break;
+            case R.id.note_back_button:
+                finish();
+                break;
+        }
     }
 
     @Override
-    public boolean onDoubleTapEvent(MotionEvent e) {
-        Log.d(TAG, "onDoubleTapEvent: double tap");
-        return false;
+    public void onBackPressed() {
+        if(mode == EDIT_MODE_ENABLED){
+            onClick(checkButton);
+        }else {
+            super.onBackPressed();
+        }
     }
 
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        return false;
+    private void hideKeyboard(){
+        InputMethodManager imm = (InputMethodManager) this.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        View view  = this.getCurrentFocus();
+        if(view == null){
+            view = new View(this);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
