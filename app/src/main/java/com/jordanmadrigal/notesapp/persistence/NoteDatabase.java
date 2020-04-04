845f7aac18a3b095bc039e1eb454380172e871ba
JMadrigal <jordan.madrigal@gmail.com>
2020-04-03 21:25:42 -0500

    Add room database singleton

---
+++ app/src/main/java/com/jordanmadrigal/notesapp/persistence/NoteDatabase.java
@@ -0,0 +1,23 @@
+package com.jordanmadrigal.notesapp.persistence;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.jordanmadrigal.notesapp.models.Note;
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    private static final String DATABASE_NAME = "notes_db";
+    private static NoteDatabase instance;
+
+    public static NoteDatabase getInstance(final Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(), NoteDatabase.class, DATABASE_NAME).build();
+        }
+        return instance;
+    }
+
+}
